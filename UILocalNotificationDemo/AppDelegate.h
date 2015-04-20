a71b0db0bd1c5e3ac3416115eab14078aa5235a8
TangJR <tangjr.work@gmail.com>
2015-04-20 09:33:39 +0800

    init

---
+++ UILocalNotificationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UILocalNotificationDemo
+//
+//  Created by TangJR on 15/4/20.
+//  Copyright (c) 2015年 tangjr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UILocalNotificationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILocalNotificationDemo
+//
+//  Created by TangJR on 15/4/20.
+//  Copyright (c) 2015年 tangjr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
